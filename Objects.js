bf89bd4d0c9657075d4e1a6f554e394cf27fa1cc
Lun-Dev <lunthedev@gmail.com>
2021-10-23 19:38:18 +0800

    Modified: Object notes

--- Objects.js
+++ Objects.js
@@ -8,3 +8,38 @@ const playerOne = {
 }
 
 console.log(playerOne.fullInfo())
+
+// Call another object
+const person1 = {
+	fullName: function() {
+  	return this.firstName + " " + this.lastName;
+  }
+}
+
+const person2 = {
+	firstName: "Jason",
+  lastName: "King"
+}
+
+console.log(person1.fullName.call(person2))
+
+// New operator (Like a template)
+function guy(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+}
+
+const dudeOne = new guy("Eli", "Manning", 43)
+console.log(dudeOne.firstName)
+
+const dudeTwo = new guy("Janet", "Dickinson", 22)
+console.log(dudeTwo.lastName)
+
+// New object from scratch
+const car = new Object();
+car.brand = "Lambo";
+car.model = "Aventador";
+car.year = 2022;
+
+console.log(car.brand)
