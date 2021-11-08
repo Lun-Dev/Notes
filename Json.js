6c403e80250604359cf64bebe047283528cf55e4
Lun-Dev <lunthedev@gmail.com>
2021-11-09 01:33:09 +0800

    Modified: Json

--- Json.js
+++ Json.js
@@ -1,8 +1,12 @@
 myLibrary = [];
 myLibrary.push({title: 'Harry Potter', author: "Rowling", pages: 50},{title: 'Scary Botter', author: "Bowling", pages: 90})
 
+const receiver = JSON.parse(localStorage.getItem("myNewLibrary")) // Good
+const receiverWrong = localStorage.getItem("myNewLibrary") // no Good
+
+
 localStorage.setItem("myNewLibrary", JSON.stringify(myLibrary))
-console.log(JSON.parse(localStorage.getItem("myNewLibrary")))
+console.log(receiver)
 /* [{
   author: "Rowling",
   pages: 50,
@@ -12,4 +16,5 @@ console.log(JSON.parse(localStorage.getItem("myNewLibrary")))
   pages: 90,
   title: "Scary Botter"
 }] */
-console.log(localStorage.getItem("myNewLibrary")) // "[{\&quot;title\&quot;:\&quot;Harry Potter\&quot;,\&quot;author\&quot;:\&quot;Rowling\&quot;,\&quot;pages\&quot;:50},{\&quot;title\&quot;:\&quot;Scary Botter\&quot;,\&quot;author\&quot;:\&quot;Bowling\&quot;,\&quot;pages\&quot;:90}]"
+console.log()
+console.log(receiverWrong) // "[{\&quot;title\&quot;:\&quot;Harry Potter\&quot;,\&quot;author\&quot;:\&quot;Rowling\&quot;,\&quot;pages\&quot;:50},{\&quot;title\&quot;:\&quot;Scary Botter\&quot;,\&quot;author\&quot;:\&quot;Bowling\&quot;,\&quot;pages\&quot;:90}]"
