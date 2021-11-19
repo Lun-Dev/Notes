8ee3ac925323c415b1b76d88daf6556b16eb143a
Lun-Dev <lunthedev@gmail.com>
2021-11-19 13:21:26 +0800

    Added: Class

---
+++ Class.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor(name) {
+        this.name = name
+        this.old = false
+    }
+}
+
+let puppy = new Dog("Toby")
+
+console.log(puppy.name) // Output: "Toby"
+console.log(puppy.old) // Output: "false"
