84ab02fc9163461f0c5c16d5deae94a75063e45d
Lun-Dev <lunthedev@gmail.com>
2021-11-22 02:13:38 +0800

    Added: Revealing Module

--- IIFE.js
+++ IIFE.js
@@ -13,3 +13,23 @@ let testFunction = (function() {
 })();
 
 testFunction.aMethod();
+
+let newFunction = (function(){
+    let _privateVariable = "I'm private"
+    let publicVariable = "I'm public"
+
+    function _privateMethod() {
+        console.log(`Hello ${_privateVariable}`)
+    }
+
+    function publicFunction() {
+        console.log(`Hello ${publicVariable}`)
+    }
+
+    return {
+        publicVariable: publicVariable,
+        publicFunction: publicFunction
+    }
+})();
+
+newFunction.publicFunction()
