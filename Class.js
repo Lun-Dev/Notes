ed37738c637cb11bdd3d61018738e7825239606f
Lun-Dev <lunthedev@gmail.com>
2021-12-01 01:41:22 +0800

    Add: Library

--- Class.js
+++ Class.js
@@ -1,23 +1,35 @@
-class Dog {
-    constructor(name) {
-        this.name = name
-        this.old = false
-    }
+// class Dog {
+//     constructor(name) {
+//         this.name = name
+//         this.old = false
+//     }
+// }
+
+// let puppy = new Dog("Toby")
+
+// // console.log(puppy.name) // Output: "Toby"
+// // console.log(puppy.old) // Output: "false"
+
+// class Person {
+//     constructor(name) {
+//         this.name = name;
+//     }
+
+//     get name() {
+//         return this.name;
+//     }
+// }
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.read = false
+  }
+    read() {
+  	    this.read = true
+  }
 }
 
-let puppy = new Dog("Toby")
-
-// console.log(puppy.name) // Output: "Toby"
-// console.log(puppy.old) // Output: "false"
-
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
-
-    get name() {
-        return this.name;
-    }
-}
-
-console.log(Person.name)
+let newBook 
