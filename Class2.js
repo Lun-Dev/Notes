b8a3f5aaa7796e752c8beeca07e79b212d9a6dcf
Lun-Dev <lunthedev@gmail.com>
2021-12-03 17:51:16 +0800

    Added: Class v2

---
+++ Class2.js
@@ -0,0 +1,36 @@
+const libraryArray = [];
+
+class Book {
+    constructor(title, author, pages, read) {
+        this._title = title;
+        this._author = author;
+        this._pages = pages;
+        this._read = false;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get pages() {
+        return this._pages;
+    }
+
+    get read() {
+        return this._read;
+    }
+}
+
+submit.addEventListener('click', function getTarget(e) {
+    e.preventDefault()
+    const titleInput = document.getElementById('title_input').value;
+    const authorInput = document.getElementById('authorinput').value;
+    const pagesInput = document.getElementById('pages_input').value;
+    const readInput = document.getElementById('read_input').value;
+    const book = new Book (titleInput, authorInput, pagesInput, readInput)
+    libraryArray.push(book)
+}
